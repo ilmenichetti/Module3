48bede668ebdacae3415686335e205a1303b679a
Lorenzo <ilmenichetti@gmail.com>
2020-11-04 19:22:32 +0100

    added metadata file and plot by region
    TODO still: reduce the plots to the last and clean the code, make a plot only by european regions and use blubble size for densities

--- basic_analysis.R
+++ basic_analysis.R
@@ -2,14 +2,20 @@
 # this file is meant just to build up the basic analysis on the data we want the students to perform
 # I will then corrupt the datafile
 
+
+library(RColorBrewer)
+
+
 ## Data read
-liberty_index<-read.csv("cliberities_fh.csv")
+#liberty_index<-read.csv("cliberities_fh.csv")
 emissions<-read.csv("co2_emissions_tonnes_per_person.csv")
 population_dens<-read.csv("population_density_per_square_km.csv")
 population<-read.csv("population_total.csv")
 deaths<-read.csv("time_series_covid19_deaths_global.csv")
-cases<-read.csv("time_series_covid19_confirmed_global.csv")
+metadata<-read.csv("Countries_metadata.csv")
 
+#TODO cleaning and reducing the amount of stuff done, focus on last plot only
+#TODO add plot only for european nations, add bubbles for density
 
 ##0 collapse the Covid data to have 1 nation only and rescale deaths by pop size
   # 0.0 collapse data
@@ -22,16 +28,6 @@ cases<-read.csv("time_series_covid19_confirmed_global.csv")
   #0.3 deaths normalized per million people
   death_by_population_DF<-data.frame(death_population_DF$deaths_agg.Group.1, death_population_DF$deaths_agg.X11.3.20/(death_population_DF$population.X2018.population_ID.*10^-6)) 
   colnames(death_by_population_DF)<-c("Group.1", "Deaths_by_million")
-
-##1  Do we have an effect of liberty on total deaths?
-  # 1.1 find which nations in the death table are matching in the liberty index table and relative indexes
-  liberty_ID<-match(death_by_population_DF$Group.1,liberty_index$country)
-  #1.2 build a combined data frame
-  # I put both ID columns to doublecheck the matching is going fine 
-  death_liberty_DF<-data.frame(death_by_population_DF$Group.1,liberty_index$country[liberty_ID], death_by_population_DF$Deaths_by_million, liberty_index$X2018[liberty_ID])
-  #1.3 plot
-  plot(death_liberty_DF$death_by_population_DF.Deaths_by_million, death_liberty_DF$liberty_index.X2018.liberty_ID., cex=0, ylab="Liberty Index", xlab="Cumulated death per million as of 3rd Oct 2020")
-  text(death_liberty_DF$death_by_population_DF.Deaths_by_million, death_liberty_DF$liberty_index.X2018.liberty_ID., death_liberty_DF$death_by_population_DF.Group.1, cex=0.5)
 
 ##2  Do we have an effect of populationd density on total deaths? And emissions?
   # 2.1 find which nations in the death table are matching in the liberty index table and relative indexes
@@ -52,4 +48,17 @@ cases<-read.csv("time_series_covid19_confirmed_global.csv")
  
  plot(emissions_death_pop_DF$death_by_population_DF.Deaths_by_million, emissions_death_pop_DF$emissions.X2018.emissions_ID., cex=0, ylab="Emissions", xlab="Cumulated death per million as of 3rd Oct 2020", ylim=c(0,40))
  text(emissions_death_pop_DF$death_by_population_DF.Deaths_by_million, emissions_death_pop_DF$emissions.X2018.emissions_ID., death_pop_DF$death_by_population_DF.Group.1, cex=0.5)
+ 
+ ##4 emissions and density and metadata
+ metadata_ID<-match(emissions_death_pop_DF$death_by_population_DF.Group.1,metadata$name)
+ emissions_death_pop_metadata_DF<-data.frame(death_pop_DF, metadata$name[metadata_ID], metadata$World.bank.region[metadata_ID], metadata$World.bank..4.income.groups.2017[metadata_ID])
+
+ #plotting the World Bank regions 
+ palette<-brewer.pal(8,"Dark2")
+ plot(emissions_death_pop_DF$death_by_population_DF.Deaths_by_million, emissions_death_pop_DF$emissions.X2018.emissions_ID., cex=0, ylab="Emissions", xlab="Cumulated death per million as of 3rd Oct 2020", ylim=c(0,40))
+ text(emissions_death_pop_DF$death_by_population_DF.Deaths_by_million, emissions_death_pop_DF$emissions.X2018.emissions_ID., death_pop_DF$death_by_population_DF.Group.1, cex=0.5, col=palette[as.factor(emissions_death_pop_metadata_DF$metadata.World.bank.region.metadata_ID.)])
+ legend("topright", levels(as.factor(emissions_death_pop_metadata_DF$metadata.World.bank.region.metadata_ID.)), col=palette, bty="n", pch=16, cex=0.8)
+ 
+ 
+ 
  
